816192965cf32c4e8b4267ca5be60328d7721310
kkang123 <one98kr@gmail.com>
2024-01-29 00:00:57 +0900

    ✨ 불필요한 주석 제거 및 navigate 수정

--- src/routes/ProtectRoute.tsx
+++ src/routes/ProtectRoute.tsx
@@ -1,41 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-
-// interface ProtectRouteProps {
-//   element: React.ReactElement; // 'component' prop을 'element' prop으로 변경하였습니다.
-//   isAuth: boolean;
-//   isPrivate?: boolean;
-// }
-
-// export const ProtectRoute: React.FC<ProtectRouteProps> = ({
-//   element,
-//   isAuth,
-//   isPrivate = false,
-// }) => {
-//   const navigate = useNavigate();
-
-//   // 경로가 비공개
-//   if (isPrivate) {
-//     // 사용자 로그인 x
-//     if (!isAuth) {
-//       console.log("비공개 경로입니다 로그인해주세요");
-//       navigate("/signup");
-//     } else {
-//       console.log("비공개 경로에 로그인 회원 접근");
-//     }
-//   } else {
-//     if (isAuth) {
-//       console.log("공개경로에 회원접근");
-//     } else {
-//       console.log("공개 경로에 비회원 접근");
-//     }
-//   }
-
-//   return element;
-// };
-
-// export default ProtectRoute;
-
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -62,7 +24,6 @@ export const ProtectRoute: React.FC<ProtectRouteProps> = ({
       console.log("비공개 경로에 로그인 회원 접근");
       if (isSeller) {
         console.log("판매자 전용 페이지 접근");
-        navigate("/seller");
       } else {
         console.log("구매자 전용 페이지 접근");
         // navigate("/"); // 구매자 전용 페이지로 수정
