e257e2171c6d70f3c686c766210d3bc6a90ce5e3
kkang123 <one98kr@gmail.com>
2024-02-02 22:26:52 +0900

    ✨ test 페이지

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,30 +1,17 @@
-import React, { FormEvent, useEffect } from "react";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/firebase";
 import { useAuth } from "@/contexts/AuthContext"; // useAuth 훅을 import
 
 import "../App.css";
 
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-
-import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import Header from "@/components/Header/MainHeader";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const authContext = useAuth();
-  console.log(authContext);
-  const { logout } = authContext;
+  const user = useAuth();
+  console.log(user);
+
   // const { logout } = useAuth(); // useAuth 훅을 사용하여 logout 함수를 얻음
 
   // 로그인 상태 확인
@@ -32,6 +19,7 @@ export default function Home() {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         console.log(true);
+        console.log(user);
       } else {
         console.log(false);
       }
@@ -39,55 +27,15 @@ export default function Home() {
     return () => unsubscribe();
   }, []);
 
-  // 로그아웃
-  const logOut = async (event: FormEvent) => {
-    event.preventDefault(); // 이벤트의 기본 동작을 막아줍니다.
-    try {
-      await signOut(auth);
-      logout(); // 로그아웃에 성공하면 logout 함수를 호출하여 isAuth 상태를 false로 변경
-      console.log("Successfully logged out");
-    } catch (error) {
-      console.error("Error during log out", error);
-    }
-  };
-
   return (
     <>
       <Header />
-      <div>
+
+      <div className="w-full h-[90vh] flex flex-col justify-start items-center mt-20">
         <h1>Home</h1>
         <p>가장 먼저 보여지는 페이지입니다.</p>
-        <button onClick={logOut}>로그아웃</button>
-      </div>
-      <div className="w-full h-screen flex flex-col justify-start items-center">
-        <Alert>
-          <AlertTitle>안녕하세요 수강생 여러분 반갑습니다.</AlertTitle>
-          <AlertDescription>
-            항해99 취업 리부트 프로그램에 오신걸 환영합니다.
-          </AlertDescription>
-        </Alert>
-
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="outline" className="mt-5">
-              버튼을 눌러주세요.
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>
-                프로젝트 셋팅을 완료하셨습니다.
-              </AlertDialogTitle>
-              <AlertDialogDescription>
-                이제 1주차 기능 구현 과제들을 구현해주세요. 화이팅입니다!
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>취소</AlertDialogCancel>
-              <AlertDialogAction>완료</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        <div>안녕하세요 {user?.nickname} 님</div>
+        <Link to={`/productdetail/1706765697233`}>123123</Link>
       </div>
     </>
   );
