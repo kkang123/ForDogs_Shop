5b11db64059d8e07e827521b8ce80e0c674a908a
kkang123 <one98kr@gmail.com>
2024-02-13 04:48:04 +0900

    ✨ 장바구니 Link 연결

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -3,7 +3,7 @@ import React, { FormEvent, useEffect, useState } from "react";
 import { auth } from "@/firebase";
 import { signOut, onAuthStateChanged } from "firebase/auth";
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
 
@@ -12,6 +12,7 @@ import basket from "@/assets/basket-buy-cart.svg";
 function MainHeader() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 저장하는 state
   const [isLoading, setIsLoading] = useState(true); // 로딩 상태를 추가
+  const [uid, setUid] = useState<string | null>(null);
   const navigate = useNavigate();
 
   // 로그인 상태 확인
@@ -20,8 +21,10 @@ function MainHeader() {
       setIsLoading(false);
       if (user) {
         setIsLoggedIn(true);
+        setUid(user.uid); // 로그인한 경우 uid를 설정
       } else {
         setIsLoggedIn(false);
+        setUid(null);
       }
     });
 
@@ -62,15 +65,19 @@ function MainHeader() {
     );
   }
 
+  console.log(`isLoggedIn: ${isLoggedIn}, uid: ${uid}`); // 로그인 상태와 uid 값을 출력
+
   return (
     <>
       <div className="fixed px-5 py-5 top-0 left-0 right-0 flex  w-full justify-between shadow-lg  bg-white z-50">
         <div className="">로고 이미지</div>
         <div className="flex">
           <div className=""></div>
-          <button className="">
-            <img src={basket} alt="Basket" />
-          </button>
+          <Link to={isLoggedIn && uid ? `/cart/${uid}` : "#"}>
+            <button className="">
+              <img src={basket} alt="Basket" />
+            </button>
+          </Link>
           <div className=" inline-block ml-2 mr-2">
             {isLoggedIn ? (
               <Button variant="outline" size="sm" onClick={logOut}>
