4b264cf6c0d5570d357d0566aaba68f5b0f21d4c
kkang123 <one98kr@gmail.com>
2024-02-23 04:15:24 +0900

    ♻️ DB 필드값 수정으로 각 상품에 대한 상품 상태 기능 구현

--- src/pages/Product/ProductManagement.tsx
+++ src/pages/Product/ProductManagement.tsx
@@ -1,314 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-
-// import { db } from "@/firebase";
-// import {
-//   collection,
-//   query,
-//   where,
-//   getDocs,
-//   Timestamp,
-//   updateDoc,
-//   doc,
-// } from "firebase/firestore";
-
-// import { useAuth } from "@/contexts/AuthContext";
-
-// import { Button } from "@/components/ui/button";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
-
-// import { Order } from "@/interface/order";
-
-// interface UserType {
-//   uid: string;
-//   email: string;
-//   isSeller: boolean;
-//   nickname: string;
-//   createdAt: Timestamp;
-//   updatedAt: Timestamp;
-// }
-
-// function ProductManagement() {
-//   const { uid } = useAuth() as { uid: string };
-//   const { uid: urlUid } = useParams<{ uid: string }>();
-//   const [orders, setOrders] = useState<Order[]>([]);
-//   const [users, setUsers] = useState<UserType[]>([]);
-
-//   useEffect(() => {
-//     console.log("로그인한 사용자의 uid: ", uid);
-
-//     const fetchUsers = async () => {
-//       const usersRef = collection(db, "users");
-//       const usersSnapshot = await getDocs(usersRef);
-//       const usersData: UserType[] = [];
-//       for (const doc of usersSnapshot.docs) {
-//         const user = doc.data() as UserType;
-//         user.uid = doc.id; // 문서 ID를 uid 필드에 저장
-//         usersData.push(user);
-//       }
-//       setUsers(usersData);
-//       console.log(usersData); // users 상태를 콘솔에 출력
-//     };
-
-//     fetchUsers();
-
-//     const fetchOrders = async () => {
-//       const ordersRef = collection(db, "orders");
-//       const querySnapshot = await getDocs(ordersRef);
-//       const fetchedOrders = querySnapshot.docs.map(
-//         (doc) => ({ id: doc.id, ...doc.data() } as Order)
-//       );
-//       const sellerOrders = fetchedOrders
-//         .map((order) => {
-//           // 판매자가 판매한 상품만 필터링
-//           const sellerItems = order.items.filter(
-//             (item) => item.product.sellerId === urlUid
-//           );
-//           return { ...order, items: sellerItems };
-//         })
-//         .filter((order) => order.items.length > 0); // 판매자가 판매한 상품이 없는 주문 제거
-//       setOrders(sellerOrders);
-//     };
-
-//     fetchOrders();
-//   }, [uid, urlUid]);
-
-//   const orderStatusOptions = [
-//     "구매 확인",
-//     "발송 대기",
-//     "발송 시작",
-//     "주문 취소",
-//     "판매 완료",
-//   ];
-
-//   const updateOrderStatus = async (orderId: string, status: string) => {
-//     const orderRef = doc(db, "orders", orderId);
-//     await updateDoc(orderRef, { status });
-//     setOrders(
-//       orders.map((order) =>
-//         order.id === orderId ? { ...order, status } : order
-//       )
-//     );
-//   };
-
-//   return (
-//     <>
-//       <div className="flex flex-wrap gap-4 ">
-//         {orders.length > 0 ? (
-//           orders.map((order) => (
-//             <div key={order.id}>
-//               <div>주문 번호: {order.id}</div>
-//               {order.items.map((item, index) => (
-//                 <div key={index}>
-//                   <div>상품 이름: {item.product.productName}</div>
-//                   <div>
-//                     판매자:{" "}
-//                     {users.find((user) => user.uid === item.product.sellerId)
-//                       ?.nickname || "알 수 없음"}
-//                   </div>
-//                   <div>상품 가격: {item.product.productPrice}</div>
-//                 </div>
-//               ))}
-//               <div>주문 상태: {order.status}</div>
-//               <Select>
-//                 <SelectTrigger className="w-[180px]">
-//                   <SelectValue placeholder={order.status} />
-//                 </SelectTrigger>
-//                 <SelectContent>
-//                   {orderStatusOptions.map((status) => (
-//                     <SelectItem
-//                       key={status}
-//                       value={status}
-//                       onSelect={() => updateOrderStatus(order.id, status)}
-//                     >
-//                       {status}
-//                     </SelectItem>
-//                   ))}
-//                 </SelectContent>
-//               </Select>
-//               <div>전체 가격: {order.amount}</div>
-//             </div>
-//           ))
-//         ) : (
-//           <div>주문이 없습니다.</div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default ProductManagement;
-
-// 2
-
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-
-// import { db } from "@/firebase";
-// import {
-//   collection,
-//   getDocs,
-//   Timestamp,
-//   updateDoc,
-//   doc,
-// } from "firebase/firestore";
-
-// import { useAuth } from "@/contexts/AuthContext";
-
-// import { Button } from "@/components/ui/button";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
-
-// import { Order } from "@/interface/order";
-
-// interface UserType {
-//   uid: string;
-//   email: string;
-//   isSeller: boolean;
-//   nickname: string;
-//   createdAt: Timestamp;
-//   updatedAt: Timestamp;
-// }
-
-// function ProductManagement() {
-//   const { uid } = useAuth() as { uid: string };
-//   const { uid: urlUid } = useParams<{ uid: string }>();
-//   const [orders, setOrders] = useState<Order[]>([]);
-//   const [users, setUsers] = useState<UserType[]>([]);
-
-//   const [selectedStatus, setSelectedStatus] = useState<Record<string, string>>(
-//     {}
-//   );
-
-//   useEffect(() => {
-//     console.log("로그인한 사용자의 uid: ", uid);
-
-//     const fetchUsers = async () => {
-//       const usersRef = collection(db, "users");
-//       const usersSnapshot = await getDocs(usersRef);
-//       const usersData: UserType[] = [];
-//       for (const doc of usersSnapshot.docs) {
-//         const user = doc.data() as UserType;
-//         user.uid = doc.id; // 문서 ID를 uid 필드에 저장
-//         usersData.push(user);
-//       }
-//       setUsers(usersData);
-//       console.log(usersData); // users 상태를 콘솔에 출력
-//     };
-
-//     fetchUsers();
-
-//     const fetchOrders = async () => {
-//       const ordersRef = collection(db, "orders");
-//       const querySnapshot = await getDocs(ordersRef);
-//       const fetchedOrders = querySnapshot.docs.map(
-//         (doc) => ({ id: doc.id, ...doc.data() } as Order)
-//       );
-//       const sellerOrders = fetchedOrders
-//         .map((order) => {
-//           // 판매자가 판매한 상품만 필터링
-//           const sellerItems = order.items.filter(
-//             (item) => item.product.sellerId === urlUid
-//           );
-//           return { ...order, items: sellerItems };
-//         })
-//         .filter((order) => order.items.length > 0); // 판매자가 판매한 상품이 없는 주문 제거
-//       setOrders(sellerOrders);
-//     };
-
-//     fetchOrders();
-//   }, [uid, urlUid]);
-
-//   const orderStatusOptions = [
-//     "구매 확인",
-//     "발송 대기",
-//     "발송 시작",
-//     "주문 취소",
-//     "판매 완료",
-//   ];
-
-//   const updateOrderStatus = async (orderId: string, status: string) => {
-//     const orderRef = doc(db, "orders", orderId);
-//     await updateDoc(orderRef, { status });
-//     setOrders(
-//       orders.map((order) =>
-//         order.id === orderId ? { ...order, status } : order
-//       )
-//     );
-//   };
-
-//   const handleStatusChange = (orderId: string, status: string) => {
-//     setSelectedStatus((prevStatus) => ({ ...prevStatus, [orderId]: status }));
-//   };
-
-//   const applyStatusChange = async (orderId: string) => {
-//     if (selectedStatus[orderId]) {
-//       await updateOrderStatus(orderId, selectedStatus[orderId]);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="flex flex-wrap gap-4 ">
-//         {orders.length > 0 ? (
-//           orders.map((order) => (
-//             <div key={order.id}>
-//               <div>주문 번호: {order.id}</div>
-//               {order.items.map((item, index) => (
-//                 <div key={index}>
-//                   <div>상품 이름: {item.product.productName}</div>
-//                   <div>
-//                     판매자:{" "}
-//                     {users.find((user) => user.uid === item.product.sellerId)
-//                       ?.nickname || "알 수 없음"}
-//                   </div>
-//                   <div>상품 가격: {item.product.productPrice}</div>
-//                 </div>
-//               ))}
-//               <div>주문 상태: {order.status}</div>
-//               <Select>
-//                 <SelectTrigger className="w-[180px]">
-//                   <SelectValue placeholder={order.status} />
-//                 </SelectTrigger>
-//                 <SelectContent>
-//                   {orderStatusOptions.map((status) => (
-//                     <SelectItem
-//                       key={status}
-//                       value={status}
-//                       onSelect={() => handleStatusChange(order.id, status)}
-//                     >
-//                       {status}
-//                     </SelectItem>
-//                   ))}
-//                 </SelectContent>
-//               </Select>
-//               <Button onClick={() => applyStatusChange(order.id)}>적용</Button>
-//               <div>전체 가격: {order.amount}</div>
-//             </div>
-//           ))
-//         ) : (
-//           <div>주문이 없습니다.</div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default ProductManagement;
-
-// 3
-
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -324,13 +13,6 @@ import {
 import { useAuth } from "@/contexts/AuthContext";
 
 import { Button } from "@/components/ui/button";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
 
 import { Order } from "@/interface/order";
 
@@ -376,16 +58,15 @@ function ProductManagement() {
       const fetchedOrders = querySnapshot.docs.map(
         (doc) => ({ id: doc.id, ...doc.data() } as Order)
       );
-      const sellerOrders = fetchedOrders
-        .map((order) => {
-          // 판매자가 판매한 상품만 필터링
-          const sellerItems = order.items.filter(
-            (item) => item.product.sellerId === urlUid
-          );
-          return { ...order, items: sellerItems };
-        })
-        .filter((order) => order.items.length > 0); // 판매자가 판매한 상품이 없는 주문 제거
-      setOrders(sellerOrders);
+      const sellerOrders = fetchedOrders.filter((order) => {
+        // 판매자가 판매한 상품만 필터링
+        return order.item.product.sellerId === uid; // 로그인 중인 회원의 uid 값과 sellerId가 동일한 상품만 필터링
+      });
+      // 주문을 구매 시간 순으로 정렬합니다.
+      const sortedSellerOrders = sellerOrders.sort((a, b) => {
+        return b.timestamp.seconds - a.timestamp.seconds;
+      });
+      setOrders(sortedSellerOrders);
     };
 
     fetchOrders();
@@ -408,20 +89,6 @@ function ProductManagement() {
       )
     );
   };
-
-  // const handleStatusChange = (orderId: string, status: string) => {
-  //   setSelectedStatus((prevStatus) => ({ ...prevStatus, [orderId]: status }));
-  // };
-
-  // const applyStatusChange = async (orderId: string) => {
-  //   if (selectedStatus[orderId]) {
-  //     try {
-  //       await updateOrderStatus(orderId, selectedStatus[orderId]);
-  //     } catch (error) {
-  //       console.error("Failed to update order status:", error);
-  //     }
-  //   }
-  // };
 
   const handleStatusChange = (orderId: string, status: string) => {
     console.log("Selected status:", status); // 선택된 상태를 출력
@@ -446,70 +113,33 @@ function ProductManagement() {
     }
   };
 
-  //   return (
-  //     <>
-  //       <div className="flex flex-wrap gap-4 ">
-  //         {orders.length > 0 ? (
-  //           orders.map((order) => (
-  //             <div key={order.id}>
-  //               <div>주문 번호: {order.id}</div>
-  //               {order.items.map((item, index) => (
-  //                 <div key={index}>
-  //                   <div>상품 이름: {item.product.productName}</div>
-  //                   <div>
-  //                     판매자:{" "}
-  //                     {users.find((user) => user.uid === item.product.sellerId)
-  //                       ?.nickname || "알 수 없음"}
-  //                   </div>
-  //                   <div>상품 가격: {item.product.productPrice}</div>
-  //                 </div>
-  //               ))}
-  //               <div>주문 상태: {order.status}</div>
-  //               <Select>
-  //                 <SelectTrigger className="w-[180px]">
-  //                   <SelectValue placeholder={order.status} />
-  //                 </SelectTrigger>
-  //                 <SelectContent>
-  //                   {orderStatusOptions.map((status) => (
-  //                     <SelectItem
-  //                       key={status}
-  //                       value={status}
-  //                       onSelect={() => handleStatusChange(order.id, status)}
-  //                     >
-  //                       {status}
-  //                     </SelectItem>
-  //                   ))}
-  //                 </SelectContent>
-  //               </Select>
-  //               <Button onClick={() => applyStatusChange(order.id)}>적용</Button>
-  //               <div>전체 가격: {order.amount}</div>
-  //             </div>
-  //           ))
-  //         ) : (
-  //           <div>주문이 없습니다.</div>
-  //         )}
-  //       </div>
-  //     </>
-  //   );
-  // }
   return (
     <>
       <div className="flex flex-wrap gap-4 ">
         {orders.length > 0 ? (
           orders.map((order) => (
             <div key={order.id}>
+              <div>
+                구매 시간:{" "}
+                {new Date(order.timestamp.seconds * 1000).toLocaleString()}
+              </div>
               <div>주문 번호: {order.id}</div>
-              {order.items.map((item, index) => (
-                <div key={index}>
-                  <div>상품 이름: {item.product.productName}</div>
-                  <div>
-                    판매자:{" "}
-                    {users.find((user) => user.uid === item.product.sellerId)
-                      ?.nickname || "알 수 없음"}
-                  </div>
-                  <div>상품 가격: {item.product.productPrice}</div>
+              <div>
+                <div>상품 이름: {order.item.product.productName}</div>
+                <div>상품 판매 갯수: {order.item.quantity}</div>
+                <div>
+                  판매자:{" "}
+                  {users.find(
+                    (user) => user.uid === order.item.product.sellerId
+                  )?.nickname || "알 수 없음"}
                 </div>
-              ))}
+                <div>
+                  구매자:{" "}
+                  {users.find((user) => user.uid === order.uid)?.nickname ||
+                    "알 수 없음"}
+                </div>
+                <div>결제 금액: {order.amount}</div>
+              </div>
               <div>주문 상태: {order.status}</div>
               <select
                 value={selectedStatus[order.id] || order.status}
@@ -522,7 +152,6 @@ function ProductManagement() {
                 ))}
               </select>
               <Button onClick={() => applyStatusChange(order.id)}>적용</Button>
-              ...
             </div>
           ))
         ) : (
