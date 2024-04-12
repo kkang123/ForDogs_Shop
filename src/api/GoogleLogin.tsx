ceaeaba43b67eaf11b757b9694993d512c9a8587
kkang123 <one98kr@gmail.com>
2024-04-12 12:41:00 +0900

    ♻️ coop 정책으로 인한 구글 소셜 로그인 방식을 리다이렉션 로그인으로 변경

--- src/api/GoogleLogin.tsx
+++ src/api/GoogleLogin.tsx
@@ -1,39 +1,90 @@
-import { auth, db } from '@/firebase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { doc, setDoc, Timestamp } from 'firebase/firestore';
-const SignIn = () => {
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then(async (result) => {
-        const user = result.user;
+// import { auth, db } from '@/firebase';
+// import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+// import { doc, setDoc, Timestamp } from 'firebase/firestore';
+// const SignIn = () => {
+//   const signInWithGoogle = () => {
+//     const provider = new GoogleAuthProvider();
+//     signInWithPopup(auth, provider)
+//       .then(async (result) => {
+//         const user = result.user;
 
-        await setDoc(
-          doc(db, 'users', user.uid),
-          {
-            email: user.email,
-            nickname: user.displayName || '익명',
-            createdAt: Timestamp.fromDate(new Date()),
-            updatedAt: Timestamp.fromDate(new Date()),
-            isSeller: false,
-          },
-          { merge: true }
-        );
-        console.log(result.user);
+//         await setDoc(
+//           doc(db, 'users', user.uid),
+//           {
+//             email: user.email,
+//             nickname: user.displayName || '익명',
+//             createdAt: Timestamp.fromDate(new Date()),
+//             updatedAt: Timestamp.fromDate(new Date()),
+//             isSeller: false,
+//           },
+//           { merge: true }
+//         );
+//         console.log(result.user);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   return (
+//     <button
+//       onClick={signInWithGoogle}
+//       className="mt-8 p-2 w-full flex justify-center border-2 border-gray-300 rounded-lg hover:bg-gray-300"
+//       aria-label="Google로 로그인"
+//     >
+//       Google Login
+//     </button>
+//   );
+// };
+
+// export default SignIn;
+
+import { useEffect } from 'react';
+import { auth, db } from '@/firebase';
+import { GoogleAuthProvider, signInWithRedirect, getRedirectResult } from 'firebase/auth';
+import { doc, setDoc, Timestamp } from 'firebase/firestore';
+
+const signInWithGoogleRedirect = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+};
+
+const SignIn = () => {
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then(async (result) => {
+        if (result) {
+          const user = result.user;
+
+          await setDoc(
+            doc(db, 'users', user.uid),
+            {
+              email: user.email,
+              nickname: user.displayName || '익명',
+              createdAt: Timestamp.fromDate(new Date()),
+              updatedAt: Timestamp.fromDate(new Date()),
+              isSeller: false,
+            },
+            { merge: true }
+          );
+          console.log(result.user);
+        }
       })
       .catch((error) => {
         console.error(error);
       });
-  };
+  }, []);
 
   return (
-    <button
-      onClick={signInWithGoogle}
-      className="mt-8 p-2 w-full flex justify-center border-2 border-gray-300 rounded-lg hover:bg-gray-300"
-      aria-label="Google로 로그인"
-    >
-      Google Login
-    </button>
+    <div>
+      <button
+        onClick={signInWithGoogleRedirect}
+        className="mt-8 p-2 w-full flex justify-center border-2 border-blue-300 rounded-lg hover:bg-blue-300"
+        aria-label="Google로 리디렉션으로 로그인"
+      >
+        Google Login
+      </button>
+    </div>
   );
 };
 
